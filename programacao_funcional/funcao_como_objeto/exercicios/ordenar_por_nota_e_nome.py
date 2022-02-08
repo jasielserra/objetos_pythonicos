fa9aa93748689dd7558feaae8cf7673bfbe19818
jasiel Serra <jasiel_serra@hotmail.com>
2022-02-08 19:31:02 -0300

    Implementado exercicio.

--- programacao_funcional/funcao_como_objeto/exercicios/ordenar_por_nota_e_nome.py
+++ programacao_funcional/funcao_como_objeto/exercicios/ordenar_por_nota_e_nome.py
@@ -3,8 +3,14 @@ Construa uma função de ordenacao que ordene os alunos por nota. Se houver empa
 deverá definir a ordem
 
 >>> alunos = [('Renzo', 0), ('Luciano', 10), ('Renzo Santos', 10), ('Renzo Nuccitelli', 10)]
+>>> import operator
+>>> ordernar_por_nota_e_nome = operator.itemgetter(1,0)
 >>> alunos.sort(key=ordernar_por_nota_e_nome)
->>> alunos
+>>> print(alunos)
 [('Renzo', 0), ('Luciano', 10), ('Renzo Nuccitelli', 10), ('Renzo Santos', 10)]
+>>> print(list(map(ordernar_por_nota_e_nome, alunos)))
+>>> [(0, 'Renzo'), (10, 'Luciano'), (10, 'Renzo Nuccitelli'), (10, 'Renzo Santos')]
 
 '''
+
+
